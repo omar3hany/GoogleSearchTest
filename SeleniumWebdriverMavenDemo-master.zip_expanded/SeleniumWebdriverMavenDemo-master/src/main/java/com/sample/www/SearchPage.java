de8cb6cf4980344b1033a790d427bdd3a77f3673
omar3hany <omar3hany@gmail.com>
2022-08-07 17:05:22 +0200

    first commit

---
+++ SeleniumWebdriverMavenDemo-master.zip_expanded/SeleniumWebdriverMavenDemo-master/src/main/java/com/sample/www/SearchPage.java
@@ -0,0 +1,19 @@
+package com.sample.www;
+
+import org.openqa.selenium.By;
+
+public class SearchPage {
+    public static By navigationNumbers = By.xpath("//div[@role='navigation']");
+    
+    
+    public static By pageTwoBtn = By.xpath("//a[@aria-label='Page 2']");
+    public static By pageThreeBtn = By.xpath("//a[@aria-label='Page 3']");
+
+    
+    public static By searchRes = By.xpath("//div[@class='g Ww4FFb vt6azd tF2Cxc']");
+
+    
+    
+
+
+}
